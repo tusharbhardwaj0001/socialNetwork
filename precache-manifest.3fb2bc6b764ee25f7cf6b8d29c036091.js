3a93a4fc5ed69824e4a4a0c79257fa73394462ea
Tushar Bhardwaj <tusharbhardwaj.313@gmail.com>
2023-01-19 23:56:55 +0530

    Updates

---
+++ precache-manifest.3fb2bc6b764ee25f7cf6b8d29c036091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9039f4157367a0147b9f",
+    "url": "/socialNetwork/static/css/main.767178fb.chunk.css"
+  },
+  {
+    "revision": "9039f4157367a0147b9f",
+    "url": "/socialNetwork/static/js/main.9039f415.chunk.js"
+  },
+  {
+    "revision": "c435bf4f4138ae44dad2",
+    "url": "/socialNetwork/static/css/1.6a555fbc.chunk.css"
+  },
+  {
+    "revision": "c435bf4f4138ae44dad2",
+    "url": "/socialNetwork/static/js/1.c435bf4f.chunk.js"
+  },
+  {
+    "revision": "3bf3c28b30d1360490a0",
+    "url": "/socialNetwork/static/js/runtime~main.3bf3c28b.js"
+  },
+  {
+    "revision": "7fe1401d88598073c104e2d750eb25f9",
+    "url": "/socialNetwork/index.html"
+  }
+];
